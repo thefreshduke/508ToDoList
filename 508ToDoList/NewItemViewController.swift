503c53b9ba717e6a965765c730d09900ba1b0e80
thefreshduke <scottyshaw1@gmail.com>
2015-09-02 04:43:18 +0100

    Created new files and connected bar buttons to Exit for unwindToList segue

---
+++ 508ToDoList/NewItemViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewItemViewController.swift
+//  508ToDoList
+//
+//  Created by Scotty Shaw on 9/2/15.
+//  Copyright (c) 2015 ___sks6___. All rights reserved.
+//
+
+import UIKit
+
+class NewItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
